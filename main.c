44e81356bbf8a9b670bfb7204a608e76fddf01ff
Deepakpant24 <123793918+Deepakpant24@users.noreply.github.com>
2023-02-03 12:41:40 +0530

    Update main.c

--- main.c
+++ main.c
@@ -7,4 +7,12 @@
  	printf("Enter the received amount : ");
  	scanf("%d",&recieved);
  	returned=received-bill;
+  int array[10]={2000,500,200,100,50,20,10,5,2,1};
+  for(int i=0;i<10;i++)
+  { 
+   int temp=returned;
+   printf("%d",temp);
+   returned=returned%a[i];
+  }
+  return 0;
  }
